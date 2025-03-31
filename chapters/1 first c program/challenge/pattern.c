f1424e06ff47a76391b94b78e845f0a76ead27f7
Biswarup Mandal <biswarupkbm@gmail.com>
2025-03-31 18:07:06 +0900

    first commit

---
+++ chapters/1 first c program/challenge/pattern.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+  printf("Right Half Pyramid");
+  printf("*\n");
+  printf("**\n");
+  printf("***\n");
+  printf("****\n");
+  printf("*****\n");
+
+  printf("Pyramid using Single print statement\n");
+  printf("*\n**\n***\n****\n*****");
+
+}
