7d60477201f898a6893d1ba861af96eff5149da9
Biswarup Mandal <biswarupkbm@gmail.com>
2025-04-04 00:22:10 +0900

    modified chapter5

--- chapters/5 itereation and loop/challenge/3factorial.c
+++ chapters/5 itereation and loop/challenge/3factorial.c
@@ -5,7 +5,14 @@ int main(){
 
   printf("Enter the number: ");
   scanf("%d", &num);
+  
+  int facts = 1;
+  for(int i=1; i<=num; i++){
+    facts *= i;
+  }
+  printf("factorial of %d is: %d\n", num, facts);
 
+  // while loop
   int i = 1;
   int facts = 1;
   while(i <= num){
--- chapters/5 itereation and loop/challenge/7prime.c
+++ chapters/5 itereation and loop/challenge/7prime.c
@@ -13,17 +13,18 @@ int main(){
     }
   }
   printf("%d is prime", num);
-
+  
   //while loop
-
+  
   int i=2;
-  while(i <= num){
+  while(i < num){
     if(num % i == 0){
       printf("%d is not prime", num);
       return 0;
     }
     i++;
   }
+  printf("%d is prime", num);
 
   return 0;
 }  
